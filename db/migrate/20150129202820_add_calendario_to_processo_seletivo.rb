32c98260a8f57c7aadb44d083f7e9b6902b07ad4
Rogério <rogeriofragoso@gmail.com>
2015-01-30 21:13:42 -0300

    Ajustando migrations para produção

--- db/migrate/20150129202820_add_calendario_to_processo_seletivo.rb
+++ db/migrate/20150129202820_add_calendario_to_processo_seletivo.rb
@@ -1,6 +1,6 @@
 class AddCalendarioToProcessoSeletivo < ActiveRecord::Migration
   def change
-    add_reference :processo_seletivos, :calendarios, index: true
-    add_foreign_key :processo_seletivos, :calendarios
+    #add_reference :processo_seletivos, :calendarios, index: true
+    #add_foreign_key :processo_seletivos, :calendarios
   end
 end
