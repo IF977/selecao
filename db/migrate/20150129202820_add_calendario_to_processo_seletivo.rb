7c2778ec4dfebcd06b4c00c10210fda465a82ce9
Rogério <rogeriofragoso@gmail.com>
2015-01-30 21:19:20 -0300

    Corrigindo erro nos dados pessoais

--- db/migrate/20150129202820_add_calendario_to_processo_seletivo.rb
+++
@@ -1,6 +0,0 @@
-class AddCalendarioToProcessoSeletivo < ActiveRecord::Migration
-  def change
-    #add_reference :processo_seletivos, :calendarios, index: true
-    #add_foreign_key :processo_seletivos, :calendarios
-  end
-end
